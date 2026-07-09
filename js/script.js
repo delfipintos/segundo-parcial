c2ae7a227e20e66068f7d49cdcd90cd52e488f63
delfipintos <oyehada@gmail.com>
2026-07-09 11:58:02 -0300

    arrays + variables + buttons script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+// arrays
+let nombres = [];
+let luces = [];
+let horas = [];
+let consumos = [];
+let costos = [];
 
+// variables
+let cantidadObras = 0;
+let cantidadIngresada = 0;
+
+// buttons
+const botonCantidad = document.querySelector("#botonCantidad");
+const botonAgregar = document.querySelector("#botonAgregar");
+const botonCalcular = document.querySelector("#botonCalcular");
+const botonReiniciar = document.querySelector("#botonReiniciar");
