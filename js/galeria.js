378508065c016a75bbbefbf8c3d44a442d26a71b
delfipintos <oyehada@gmail.com>
2026-07-09 20:18:51 -0300

    data input galeria.js

--- js/galeria.js
+++ js/galeria.js
@@ -1,1 +1,22 @@
+// arrays
 
+let nombres = [ 
+  "pulse room",
+  "vectorial elevation",
+  "33 questions per minute",
+  "body movies"
+]; 
+
+let anios = [ 
+  2006,
+  1999,
+  2000,
+  2002
+];
+
+let imagenes = [ 
+  // lozano-hemmer-3 
+  // subir imagen
+  // subir imagen
+  // subir imagen
+];
